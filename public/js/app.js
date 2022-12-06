059a674691577681e5fe52eafb72b4f8641f2adb
ysh <devilsyash19@gmail.com>
2022-12-06 17:12:07 +0000

    Update 4 files

--- public/js/app.js
+++ public/js/app.js
@@ -49485,8 +49485,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /usr/share/nginx/html/example/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /usr/share/nginx/html/example/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /project/laravel_neo_api/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /project/laravel_neo_api/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
